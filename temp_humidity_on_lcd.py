f40862689c8d3f92267b5cdc9148fb40bfc69b81
Alessandro Tuttobene <t.alessandro@web.de>
2020-04-05 13:18:37 +0200

    Added first scripts

---
+++ temp_humidity_on_lcd.py
@@ -0,0 +1,70 @@
+import time,sys
+import grovepi
+import math
+
+if sys.platform == 'uwp':
+    import winrt_smbus as smbus
+    bus = smbus.SMBus(1)
+else:
+    import smbus
+    import RPi.GPIO as GPIO
+    rev = GPIO.RPI_REVISION
+    if rev == 2 or rev == 3:
+        bus = smbus.SMBus(1)
+    else:
+        bus = smbus.SMBus(0)
+
+# this device has two I2C addresses
+DISPLAY_RGB_ADDR = 0x62
+DISPLAY_TEXT_ADDR = 0x3e
+
+# set backlight to (R,G,B) (values from 0..255 for each)
+def setRGB(r,g,b):
+    bus.write_byte_data(DISPLAY_RGB_ADDR,0,0)
+    bus.write_byte_data(DISPLAY_RGB_ADDR,1,0)
+    bus.write_byte_data(DISPLAY_RGB_ADDR,0x08,0xaa)
+    bus.write_byte_data(DISPLAY_RGB_ADDR,4,r)
+    bus.write_byte_data(DISPLAY_RGB_ADDR,3,g)
+    bus.write_byte_data(DISPLAY_RGB_ADDR,2,b)
+
+# send command to display (no need for external use)    
+def textCommand(cmd):
+    bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
+
+#Update the display without erasing the display
+def setText_norefresh(text):
+    textCommand(0x02) # return home
+    time.sleep(.05)
+    textCommand(0x08 | 0x04) # display on, no cursor
+    textCommand(0x28) # 2 lines
+    time.sleep(.05)
+    count = 0
+    row = 0
+    while len(text) < 32: #clears the rest of the screen
+        text += ' '
+    for c in text:
+        if c == '\n' or count == 16:
+            count = 0
+            row += 1
+            if row == 2:
+                break
+            textCommand(0xc0)
+            if c == '\n':
+                continue
+        count += 1
+        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+
+if __name__=="__main__":
+    sensor = 8
+    setRGB(5,5,5)
+
+    while True:
+        try:
+            [temp,humidity] = grovepi.dht(sensor,0)
+            if math.isnan(temp) == False and math.isnan(humidity) == False:
+                setText_norefresh("Temp: %.0f Grad   Humidity: %.0f%%"%(temp,humidity))
+
+            time.sleep(0.1)
+
+        except IOError:
+            print("Error")
