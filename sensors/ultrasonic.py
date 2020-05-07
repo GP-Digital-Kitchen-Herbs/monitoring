52223b4b76d2f1c322f95106066c549e60b8da7b
Bettlaken <57103947+Bettlaken@users.noreply.github.com>
2020-05-07 16:12:47 +0200

    added median in ultra sonic ranger measurement (#7)

--- sensors/ultrasonic.py
+++ sensors/ultrasonic.py
@@ -1,6 +1,12 @@
 import services.telemetry as telemetry
 from datetime import datetime
 from grovepi import *
+from statistics import median
+
+# 10 seconds would probably also work, but just in case 15 to not brick the sensor / board
+INTERVAL = 15
+# Number of measurements
+MEASUREMENT_COUNT = 3
 
 def monitoreDistance(currentConfig):
 
@@ -18,9 +24,15 @@ def monitoreDistance(currentConfig):
 def sendDistance(currentConfig):
     sensor = currentConfig["sensor_ultra_sonic"]
     if currentConfig["null_distance"]:
-        sensor_value = int(currentConfig["null_distance"]) - ultrasonicRead(sensor)
+        sensor_values = []
+        for i in range(MEASUREMENT_COUNT):
+            sensor_values.append(int(currentConfig["null_distance"]) - ultrasonicRead(sensor))
+            time.sleep(INTERVAL)
+        sensor_value = median(sensor_values)
         print('water:', sensor_value, 'cm')
         data = {'water' : sensor_value}
         telemetry.sendTelemetry(data)
     else:
         print("Please calibrate the Ultrasonic-Ranger")
+    
+
